f821904c7e039b4b758fe1862812baf9b03c79b6
Vincent Biabiany <vincentbiabiany@live.fr>
2017-10-30 14:21:25 +0100

    debug

---
+++ web/js/custom-file-input.js
@@ -0,0 +1,22 @@
+var inputs = document.querySelectorAll( '.input-file' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+			fileName = e.target.value.split( '\\' ).pop();
+		if( fileName ){
+            if (fileName.length > 28){
+              fileName = jQuery.trim(fileName).substring(0, 20).split(" ").slice(0, -1).join(" ") + "...";
+              label.querySelector( 'span' ).innerHTML = fileName;
+            }else{
+              label.querySelector( 'span' ).innerHTML = fileName;
+            }
+
+        }	else{
+    			label.innerHTML = labelVal;
+           }
+	});
+});
