e389a4f4ab319fdd867896243570889078fc9359
Vincent Biabiany <vincentbiabiany@live.fr>
2017-11-09 12:03:47 +0100

    Cleaning js

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
 			new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Liuggio\ExcelBundle\LiuggioExcelBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 
         ];
 
