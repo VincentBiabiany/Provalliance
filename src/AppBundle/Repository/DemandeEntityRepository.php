2bf89418803bd7747c3febce8966736a19662628
Adrcpp <adrcpp@gmail.com>
2017-11-19 15:15:33 +0100

    Correction des filtres dans suivi des demandes

--- src/AppBundle/Repository/DemandeEntityRepository.php
+++ src/AppBundle/Repository/DemandeEntityRepository.php
@@ -23,18 +23,19 @@ class DemandeEntityRepository extends \Doctrine\ORM\EntityRepository
     }
 
     $col = array(
-      "id",
-      "",
-      "sage",
-      "enseigne",
-      "appelation",
-      "coordinateur",
-      "manager",
-      "dateTraitement",
-      "statut",
-      "type",
-      "collaborateur",
-    );
+                  "id",
+                  "",
+                  "sage",
+                  "enseigne",
+                  "appelation",
+                  "coordinateur",
+                  "manager",
+                  "dateTraitement",
+                  "statut",
+                  "type",
+                  "collaborateur",
+                );
+
     // Récupération des filtres
     foreach ($filter as $key => $value) {
       $colFilter[] = $col[$key];
@@ -43,23 +44,21 @@ class DemandeEntityRepository extends \Doctrine\ORM\EntityRepository
     $query = $this->createQueryBuilder('d')
                   ->select('d');
 
-    if ($role == 'ROLE_PAIE') {
-      $query = $query->andWhere('d.service = :service')->setParameter('service', 'paie');
-    }
+    // if ($role == 'ROLE_PAIE') {
+    //   $query = $query->andWhere('d.service = :service')->setParameter('service', 'paie');
+    // }
 
     if ($role == 'ROLE_JURIDIQUE') {
-      $query = $query->andWwhere('d.service = :service')->setParameter('service', 'juridique');
+      $query = $query->andWhere('d.service = :service')->setParameter('service', 'juridique');
     }
 
-    if (in_array("sage", $colFilter) || $isService == false)
-    {
+    if (in_array("sage", $colFilter) || $isService == false) {
       in_array("sage", $colFilter) ? $salon = $filter[2] : $salon = $idSalon;
       $query = $query->andWhere('d.idSalon = :salon')->setParameter('salon', $salon);
     }
 
 
-    if (in_array("enseigne", $colFilter) && in_array("appelation", $colFilter))
-    {
+    if (in_array("enseigne", $colFilter) && in_array("appelation", $colFilter)) {
       $salons = $em->createQueryBuilder('u')
                    ->select('u.sage as id')
                    ->from('ApiBundle:Salon', 'u')
@@ -84,29 +83,30 @@ class DemandeEntityRepository extends \Doctrine\ORM\EntityRepository
        $query = $query->andWhere('d.idSalon IN (:id)')->setParameter('id', $result);
 
     } else {
-      if (in_array("enseigne", $colFilter))
-      {
+
+      if (in_array("enseigne", $colFilter)) {
         $salons = $em->createQueryBuilder('u')
                      ->select('u.sage as id')
                      ->from('ApiBundle:Salon', 'u')
                      ->leftjoin('u.enseigne', 'e')
                      ->andWhere('e.nom = :nom')
-                     ->setParameter('nom', $filter[3]);
+                     ->setParameter('nom', $filter[3])->getQuery()->getResult();;
+
 
         foreach ($salons as $key => $value) {
           $salonsId[] = $value['id'];
         }
         if (isset($salonsId))
           $query = $query->andWhere('d.idSalon IN (:id)')->setParameter('id', $salonsId);
+
       }
 
-      if (in_array("appelation", $colFilter))
-      {
+      if (in_array("appelation", $colFilter)) {
         $salons = $em->createQueryBuilder('u')
                      ->select('u.sage as id')
                      ->from('ApiBundle:Salon', 'u')
                      ->where('u.appelation = :appelation')
-                     ->setParameter('appelation', $filter[4]);
+                     ->setParameter('appelation', $filter[4])->getQuery()->getResult();;
 
          foreach ($salons as $key => $value) {
                $salonsId[] = $value['id'];
@@ -116,40 +116,50 @@ class DemandeEntityRepository extends \Doctrine\ORM\EntityRepository
       }
     }
 
-    if (in_array("coordinateur", $colFilter))
-    {
-      // 1 Retrouver id Personnel à partir du prenom + nom
-      $idPerso = $em->createQueryBuilder('u')
-                   ->select('s.sage as id')
-                   ->from('ApiBundle:Personnel', 'u')
-                   ->where("CONCAT(u.prenom, ' ', u.nom) = :name")
-                   ->leftjoin('u.salon', 's')
-                   ->setParameter('name', $filter[5])->getQuery()->getResult();
-     foreach ($idPerso as $key => $value) {
+    if (in_array("coordinateur", $colFilter)) {
+      // Récupération de l'id du coordinateur
+      // via son nom + prénom
+      $idPerso = $em->createQueryBuilder('p')
+                    ->select('p.matricule as id')
+                    ->from('ApiBundle:Personnel', 'p')
+                    ->where("CONCAT(p.prenom, ' ', p.nom) = :name")
+                    ->setParameter('name', $filter[5])->getQuery()->getSingleResult();
+
+      // Récupération des ids des salons
+      // du coordinateur
+      $idSalon = $em->createQueryBuilder('s')
+                    ->select('u.sage as id')
+                    ->from('ApiBundle:PersonnelHasSalon', 's')
+                    ->where("s.personnelMatricule = :id")
+                    ->leftjoin('s.salonSage', 'u')
+                    ->setParameter('id', $idPerso['id'])->getQuery()->getResult();
+
+
+      foreach ($idSalon as $key => $value) {
         $ids[] = $value['id'];
-     }
-     $query = $query->andWhere('d.idSalon IN (:id)')->setParameter('id', $ids);
-    }
-
-    if (in_array("manager", $colFilter))
-    {
-      $idPerso = $em->createQueryBuilder('u')
-                   ->select('u.matricule as id')
-                   ->from('ApiBundle:Personnel', 'u')
-                   ->andWhere("CONCAT(u.prenom, ' ', u.nom) = :name")
-                   ->setParameter('name', $filter[6])->getQuery()->getResult();
-
-       foreach ($idPerso as $key => $value) {
-          $idP[] = $value['id'];
-       }
-
-       $query = $query->leftjoin('d.user', 'e')
-                      ->andwhere('e.matricule = :id')
-                      ->setParameter('id', $idP);
       }
 
-    if (in_array("collaborateur", $colFilter))
-    {
+      $query = $query->andWhere('d.idSalon IN (:idSalon)')->setParameter('idSalon', $ids);
+    }
+
+    if (in_array("manager", $colFilter)) {
+      $idPerso = $em->createQueryBuilder('u')
+                    ->select('u.matricule as id')
+                    ->from('ApiBundle:Personnel', 'u')
+                    ->andWhere("CONCAT(u.prenom, ' ', u.nom) = :name")
+                    ->setParameter('name', $filter[6])->getQuery()->getResult();
+
+      foreach ($idPerso as $key => $value) {
+        $idP[] = $value['id'];
+      }
+
+      $query = $query->leftjoin('d.user', 'e')
+                     ->andwhere('e.matricule = :idPerso')
+                     ->setParameter('idPerso', $idP);
+    }
+
+    if (in_array("collaborateur", $colFilter)) {
+
       $demandesSalon = $this->findAll();
       foreach ($demandesSalon as $key => $demande) {
         if ($demande->getDemandeform()->getTypeForm() == "Demande d'embauche") {
@@ -161,19 +171,18 @@ class DemandeEntityRepository extends \Doctrine\ORM\EntityRepository
               $demandeId[] =  $demande->getId();
         }
       }
+
       $query = $query->andWhere('d.id IN (:id)')->setParameter('id', $demandeId);
     }
 
-    if (in_array("type", $colFilter))
-    {
-      $query = $query
-                    ->leftjoin('d.demandeform', 't')
-                    ->andWhere('t.typeForm = :type')
-                    ->setParameter('type', $filter[9]);
+    if (in_array("type", $colFilter)) {
+
+      $query = $query->leftjoin('d.demandeform', 't')
+                     ->andWhere('t.typeForm = :type')
+                     ->setParameter('type', $filter[9]);
     }
 
-    if (in_array("statut", $colFilter))
-    {
+    if (in_array("statut", $colFilter)) {
       if ($filter[8] == "Rejeté")
         $statut = 0;
       else if ($filter[8] == "En cours")
@@ -184,10 +193,11 @@ class DemandeEntityRepository extends \Doctrine\ORM\EntityRepository
         $statut = 3;
       else
         $statut = 4;
+
       $query = $query->andWhere('d.statut = :stat')->setParameter('stat', $statut);
     }
 
-    if (in_array("dateTraitement", $colFilter)){
+    if (in_array("dateTraitement", $colFilter)) {
       $date = new \DateTime();
       $date = $date->createFromFormat('d/m/Y', $filter[7]);
 
